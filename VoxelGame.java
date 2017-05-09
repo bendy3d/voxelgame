427ce7200924e31d5445171e6861ba3838f2e6d9
Andrew Niklas <acniklas@cpp.edu>
2017-05-09 10:23:57 -0700

    Added code for applying textures
    Signed-off-by: Andrew Niklas <acniklas@cpp.edu>

--- VoxelGame.java
+++ VoxelGame.java
@@ -70,6 +70,10 @@ public class VoxelGame {
 		glEnable(GL_DEPTH_TEST);
 		glMatrixMode(GL_MODELVIEW);
 		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+		
+		//for texturing
+		glEnable(GL_TEXTURE_2D);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	}
 
 }
